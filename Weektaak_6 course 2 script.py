e32dc807f56033649f52f7863db714483dfd642c
Daria Gajewska <DO.Gajewska@student.han.nl>
2021-12-14 14:49:30 +0100

    .

--- Weektaak_6 course 2 script.py
+++ Weektaak_6 course 2 script.py
@@ -2,11 +2,19 @@ def openbestand():
     '''opent het bestand
     input: het excel bestand
     '''
-    open_bestand = open(bestand)
+    open_bestand = open(bestand, 'r')
     read = open_bestand.readline().split().strip()
-    #for i in open_bestand:
+    for i in open_bestand:
+        regel = i.split('\t')
+        # variation reads =< 5
+        # SNP state is leeg (blank)
+        # Synonomous = 'FALSE'
+        # if 'Retinitis' in OMIM_DISEASE
+        # Gene component = SA_SITE, EXON_REGION
 
-    # ...
+        if 'Retinitis' in regel[]:
+
+
 
 def soorteren():
     '''filtrert het excel bestand
