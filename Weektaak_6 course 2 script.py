755a7d357399e972df4e6c3ed883807fcbe6c310
Daria Gajewska <DO.Gajewska@student.han.nl>
2021-12-14 17:33:34 +0100

    .

--- Weektaak_6 course 2 script.py
+++ Weektaak_6 course 2 script.py
@@ -11,12 +11,15 @@ def soorteren(bestand):
     for i in open_bestand:
         regel = i.strip().split('\t')
 
-        if int(regel[6]) >= 5:
-            if regel[13] == '':
-                if regel[44] == 'FALSE':
-                    if 'Retinitis' in regel:
+        if len(regel) > 61:
+            if int(regel[6]) >= 5:
+                if regel[13] == '':
+                    if regel[44] == 'FALSE':
                         if regel[33] == 'SA_SITE' or regel[33] == 'EXON_REGION':
-                            lijst.append(regel)
+                            if 'Retinitis' in regel:
+                                lijst.append(regel)
+                        else:
+                            continue
                     else:
                         continue
                 else:
@@ -30,6 +33,7 @@ def soorteren(bestand):
             # Synonomous = 'FALSE'
             # if 'Retinitis' in OMIM_DISEASE
             # Gene component = SA_SITE, EXON_REGION
+
     open_bestand.close()
     print(lijst)
 
