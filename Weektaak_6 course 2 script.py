d5e387ba0d071ef5d0896958121a35a48c3c2181
Daria Gajewska <DO.Gajewska@student.han.nl>
2021-12-14 15:25:54 +0100

    .

--- Weektaak_6 course 2 script.py
+++ Weektaak_6 course 2 script.py
@@ -3,12 +3,24 @@ def openbestand(bestand):
     input: het excel bestand
     '''
     open_bestand = open(bestand, 'r')
-    read = open_bestand.readline()
+    #read = open_bestand.readline()
     for i in open_bestand:
-        #for i in range(0, 5):
-        regel = i.strip().split('\t')
-        #print(i)
+        regel = i.split('\t')
         print(regel)
+        if regel[6] => 5:
+            if regel[13] == '':
+                if regel[44] == 'FALSE':
+                    if 'Retinitis' in regel[61]:
+                        if regel[33] == 'SA_SITE' or regel[33] == 'EXON_REGION':
+
+                    else:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+        else:
+            continue
             # variation reads =< 5
             # SNP state is leeg (blank)
             # Synonomous = 'FALSE'
