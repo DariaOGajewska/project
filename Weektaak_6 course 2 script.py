53f698542d2eb56731fcddf18af56358cacd0aa3
Daria Gajewska <DO.Gajewska@student.han.nl>
2021-12-14 14:56:23 +0100

    .

--- Weektaak_6 course 2 script.py
+++ Weektaak_6 course 2 script.py
@@ -3,15 +3,17 @@ def openbestand(bestand):
     input: het excel bestand
     '''
     open_bestand = open(bestand, 'r')
-    read = open_bestand.readline().split().strip()
+    read = open_bestand.readline()
     for i in open_bestand:
-        regel = i.split('\t').strip()
-        print(regel[0:3])
-        # variation reads =< 5
-        # SNP state is leeg (blank)
-        # Synonomous = 'FALSE'
-        # if 'Retinitis' in OMIM_DISEASE
-        # Gene component = SA_SITE, EXON_REGION
+        #for i in range(0, 5):
+        regel = i.strip().split('\t')
+    print(i)
+    print(regel)
+            # variation reads =< 5
+            # SNP state is leeg (blank)
+            # Synonomous = 'FALSE'
+            # if 'Retinitis' in OMIM_DISEASE
+            # Gene component = SA_SITE, EXON_REGION
 
 
 def soorteren():
